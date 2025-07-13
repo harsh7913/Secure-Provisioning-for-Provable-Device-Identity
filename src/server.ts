afdf615a76d38cd32cefe84f47f7eeed1ec8c1e4
harsh7913 <workmailharsh@gmail.com>
2025-07-13 09:08:40 +0530

    load environment variables using dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import app from './app';
 import './jobs/worker';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
